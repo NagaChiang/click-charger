e5cb293fc670e6d458198c5268f61d44201d9798
Naga Chiang <nagachiangdev@gmail.com>
2021-01-29 11:23:38 +0800

    Refactor AnimatedNumbetText with TweenAnimationBuilder

--- lib/widgets/animated_number_text.dart
+++ lib/widgets/animated_number_text.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-class AnimatedNumberText extends ImplicitlyAnimatedWidget {
+class AnimatedNumberText extends StatefulWidget {
   final int number;
   final Duration duration;
   final TextStyle style;
@@ -12,29 +12,27 @@ class AnimatedNumberText extends ImplicitlyAnimatedWidget {
     this.style,
     this.postString,
   })  : assert(number != null),
-        assert(duration != null),
-        super(duration: duration);
+        assert(duration != null);
 
   @override
   _AnimatedNumberTextState createState() => _AnimatedNumberTextState();
 }
 
-class _AnimatedNumberTextState
-    extends AnimatedWidgetBaseState<AnimatedNumberText> {
-  Tween<double> _numberTween;
+class _AnimatedNumberTextState extends State<AnimatedNumberText> {
+  double _currentValue = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      _numberTween.evaluate(animation).floor().toString() +
-          (widget.postString ?? ''),
-      style: widget.style ?? DefaultTextStyle.of(context).style,
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: _currentValue, end: widget.number.toDouble()),
+      duration: widget.duration,
+      builder: (context, value, child) {
+        _currentValue = value;
+        return Text(
+          _currentValue.floor().toString() + (widget.postString ?? ''),
+          style: widget.style ?? DefaultTextStyle.of(context).style,
+        );
+      },
     );
-  }
-
-  @override
-  void forEachTween(TweenVisitor<dynamic> visitor) {
-    _numberTween = visitor(_numberTween, widget.number,
-        (dynamic value) => Tween<double>(begin: value as double));
   }
 }
