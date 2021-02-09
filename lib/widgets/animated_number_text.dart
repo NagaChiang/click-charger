f9a9f12e388fd96253631436010223b7c8df004a
Naga Chiang <nagachiangdev@gmail.com>
2021-02-09 11:26:55 +0800

    Add snapping threshold to AnimatedNumberText

--- lib/widgets/animated_number_text.dart
+++ lib/widgets/animated_number_text.dart
@@ -21,6 +21,8 @@ class AnimatedNumberText extends StatefulWidget {
 }
 
 class _AnimatedNumberTextState extends State<AnimatedNumberText> {
+  static const int _threshold = 1;
+
   double _currentValue = 0;
 
   @override
@@ -29,7 +31,12 @@ class _AnimatedNumberTextState extends State<AnimatedNumberText> {
       tween: Tween<double>(begin: _currentValue, end: widget.number.toDouble()),
       duration: widget.duration,
       builder: (context, value, child) {
-        _currentValue = value;
+        if (widget.number.toDouble() - _currentValue <= _threshold) {
+          _currentValue = widget.number.toDouble();
+        } else {
+          _currentValue = value;
+        }
+
         return Text(
           Utils.toFormattedNumber(_currentValue.floor()) +
               (widget.postString ?? ''),
