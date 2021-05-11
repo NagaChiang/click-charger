c7c41143a7f49da36fa533b3667e053bf634ebbb
Naga Chiang <nagachiangdev@gmail.com>
2021-05-11 23:28:35 +0800

    Clear all notifications onStop()

--- android/app/src/main/kotlin/com/example/flutter_clicker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_clicker/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.timespawn.click_charger
 
+import android.app.NotificationManager
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onStop() {
+        super.onResume()
+        closeAllNotifications();
+    }
+
+    private fun closeAllNotifications() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelAll()
+    }
 }
